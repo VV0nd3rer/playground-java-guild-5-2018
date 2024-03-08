84c7b3ab0ff8b209ee15ee5dd34079bee5cee168
kverchi <kverchi24@gmail.com>
2024-03-08 19:14:15 +0100

    Letter count solution

--- java-project/src/main/java/streams/LetterCount.java
+++ java-project/src/main/java/streams/LetterCount.java
@@ -4,12 +4,9 @@ package streams;
 import java.util.stream.*;
 
 public class LetterCount {
-// }
 
 public static int getTotalNumberOfLettersOfNamesLongerThanFive(String... names) {
-	return 0;
+	return Stream.of(names).filter(n -> n.length() > 5).mapToInt(String::length).sum();
 }
 
-//{ autofold
 }
-//}
