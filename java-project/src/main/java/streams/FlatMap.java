c8858cc0091401f41ecfeec85400c4732f7cd4ab
kverchi <kverchi24@gmail.com>
2024-03-08 19:41:46 +0100

    Flat map solution

--- java-project/src/main/java/streams/FlatMap.java
+++ java-project/src/main/java/streams/FlatMap.java
@@ -2,6 +2,7 @@
 package streams;
 
 import java.util.*;
+import java.util.stream.Stream;
 
 import static java.util.stream.Collectors.*;
 
@@ -9,13 +10,10 @@ public class FlatMap {
 // }
 
 public static List<String> transform(List<List<String>> collection) {
-	List<String> newCollection = new ArrayList<>();
-	for (List<String> subCollection : collection) {
-		for (String value : subCollection) {
-			newCollection.add(value);
-		}
-	}
-	return newCollection;
+
+	// https://stackoverflow.com/questions/39874242/what-is-the-difference-between-stream-and-stream-of
+	// return Stream.of(collection).flatMap(item -> Stream.of(item)).collect(toList());
+	return collection.stream().flatMap(Collection::stream).collect(toList());
 }
 
 // { autofold
