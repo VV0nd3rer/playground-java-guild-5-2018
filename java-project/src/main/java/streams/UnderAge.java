2d2e3d114964ae5f097c5430c2d69986a5ec3125
kverchi <kverchi24@gmail.com>
2024-03-28 22:48:19 +0100

    Get the names of all kids

--- java-project/src/main/java/streams/UnderAge.java
+++ java-project/src/main/java/streams/UnderAge.java
@@ -2,11 +2,12 @@
 package streams;
 
 import java.util.*;
+import java.util.stream.Collectors;
 
 public class UnderAge {
 // }
 
-	public static Set<String> getKidNames(List<Person> people) {
+	public static Set<String> getKidNamesV7(List<Person> people) {
 		Set<String> kids = new HashSet<>();
 		for (Person person : people) {
 			if (person.getAge() < 18) {
@@ -14,6 +15,13 @@ public class UnderAge {
 			}
 		}
 		return kids;
+	}
+
+	public static Set<String> getKidNames(List<Person> people) {
+		return people.stream()
+				.filter(person -> person.getAge() < 18)
+				.map(Person::getName)
+				.collect(Collectors.toSet());
 	}
 
 // { autofold
