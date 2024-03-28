2e3ee253364abcdf5e33600e0439b72592ba5fb7
kverchi <kverchi24@gmail.com>
2024-03-28 23:00:59 +0100

    Sum all elements of a collection

--- java-project/src/main/java/streams/SumAndReduce.java
+++ java-project/src/main/java/streams/SumAndReduce.java
@@ -6,12 +6,16 @@ import java.util.*;
 public class SumAndReduce {
 // }
 
-	public static int calculate(List<Integer> numbers) {
+	public static int calculateV7(List<Integer> numbers) {
 		int total = 0;
 		for (int number : numbers) {
 			total += number;
 		}
 		return total;
+	}
+
+	public static int calculate(List<Integer> numbers) {
+		return numbers.stream().reduce(0, Integer::sum);
 	}
 
 // { autofold
