35b3e46008cb18f4d6faf5fc65b9087840514bd3
kverchi <kverchi24@gmail.com>
2024-06-11 22:52:10 +0200

    Return a string based on a given list of integers

--- java-project/src/main/java/streams/Challenge.java
+++ java-project/src/main/java/streams/Challenge.java
@@ -2,6 +2,7 @@
 package streams;
 
 import java.util.*;
+import java.util.stream.Collectors;
 
 import static java.util.stream.Collectors.*;
 
@@ -9,7 +10,8 @@ public class Challenge {
 // }
 
 public static String getString(List<Integer> list) {
-	return null;
+	return list.stream().map(number -> number % 2 == 0 ? "e" + number : "o" + number)
+			.collect(Collectors.joining(","));
 }
 
 // { autofold
