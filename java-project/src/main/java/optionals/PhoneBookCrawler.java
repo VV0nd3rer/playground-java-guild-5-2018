168406725b3626355a57c6d4109cfd761038c83e
kverchi <kverchi24@gmail.com>
2025-03-06 02:02:13 +0100

    Implement findPhoneNumberByNameOrNameByPhoneNumber

--- java-project/src/main/java/optionals/PhoneBookCrawler.java
+++ java-project/src/main/java/optionals/PhoneBookCrawler.java
@@ -16,11 +16,15 @@ public class PhoneBookCrawler {
     }
 
     public String findPhoneNumberByNameAndPrintPhoneBookIfNothingFound(String name){
-        return phoneBook.findPhoneNumberByName(name).orElseGet(() -> phoneBook.toString());
+//        return phoneBook.findPhoneNumberByName(name).orElseGet(() -> phoneBook.toString());
+        return phoneBook.findPhoneNumberByName(name).orElse(phoneBook.toString());
     }
 
     public String findPhoneNumberByNameOrNameByPhoneNumber(String name, String phoneNumber){
-        return null;
+        return phoneBook.findPhoneNumberByName(name)
+                .orElseGet(() -> phoneBook.findNameByPhoneNumber(phoneNumber)
+                        .orElseGet(() -> phoneBook.findPhoneNumberByName("Jos de Vos").get()));
+
     }
 
     public PhoneBook getPhoneBook(){
